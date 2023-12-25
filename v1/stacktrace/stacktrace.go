d194ff1b574d0fdf76138542ea59bfd1d505e8fe
bankonly <110370337+mrbankonly@users.noreply.github.com>
2023-12-25 16:09:50 +0700

    0.2.41

--- v1/stacktrace/stacktrace.go
+++ v1/stacktrace/stacktrace.go
@@ -9,3 +9,11 @@ var (
 func BadRequest(message string) error {
 	return errors.New("400" + MatchString + message)
 }
+
+func Unauthorized(message string) error {
+	return errors.New("401" + MatchString + message)
+}
+
+func InternalServerError(message string) error {
+	return errors.New("401" + MatchString + message)
+}
